2eee11cd2456fbdc2d795767fe6469603229bf4e
wtarit <wtarit@proton.me>
2023-05-31 00:44:14 +0700

    add mT5 code

---
+++ mT5/trainmt5_tokenized_dataset.py
@@ -0,0 +1,79 @@
+from transformers import AutoTokenizer
+from datasets import load_from_disk
+from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer, DataCollatorForSeq2Seq
+import numpy as np
+import evaluate
+import transformers
+
+
+checkpoint = "google/mt5-small"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
+data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
+metric = evaluate.load("sacrebleu")
+
+
+tokenized_sentence = load_from_disk("../data/scb-mt-hf-dataset-tokenized")
+
+
+def postprocess_text(preds, labels):
+    preds = [pred.strip() for pred in preds]
+    labels = [[label.strip()] for label in labels]
+
+    return preds, labels
+
+
+def compute_metrics(eval_preds):
+    preds, labels = eval_preds
+    if isinstance(preds, tuple):
+        preds = preds[0]
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    decoded_preds, decoded_labels = postprocess_text(
+        decoded_preds, decoded_labels)
+
+    result = metric.compute(predictions=decoded_preds,
+                            references=decoded_labels)
+    result = {"bleu": result["score"]}
+
+    prediction_lens = [np.count_nonzero(
+        pred != tokenizer.pad_token_id) for pred in preds]
+    result["gen_len"] = np.mean(prediction_lens)
+    result = {k: round(v, 4) for k, v in result.items()}
+    return result
+
+
+transformers.logging.set_verbosity_info()
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir="mt5-small-scb-mt-th-en-cleaned-bf16",
+    evaluation_strategy="epoch",
+    learning_rate=5e-5,
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
+    gradient_accumulation_steps=2,
+    weight_decay=0.01,
+    save_total_limit=3,
+    num_train_epochs=5,
+    predict_with_generate=True,
+    bf16=True,
+    push_to_hub=False,
+    report_to="none",
+    run_name="mt5-small-lr5e-5"
+)
+
+trainer = Seq2SeqTrainer(
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=tokenized_sentence["train"],
+    eval_dataset=tokenized_sentence["test"],
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+trainer.save_model("mt5-small-scb")
